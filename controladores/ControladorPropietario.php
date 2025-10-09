12b78d42f83bd2022e0d682004279511da6f4309
Pilar <pilarcosta123@gmail.com>
2025-10-09 00:00:11 -0300

    agregado ModeloPropietario y ControladorPropietario

---
+++ controladores/ControladorPropietario.php
@@ -0,0 +1,72 @@
+<?php
+require_once './modelos/ModeloPropietario.php';
+require_once './vistas/VistaPropietario.php';
+
+class ControladorPropietario {
+    private $modelo;
+    private $vista;
+
+    public function __construct() {
+        $this->modelo = new ModeloPropietario();
+        $this->vista = new VistaPropietario(); 
+    }
+
+    public function mostrarPropietarios() {
+        $propietarios = $this->modelo->obtenerPropietarios();
+        $this->vista->mostrarPropietarios($propietarios);
+    }
+
+    public function mostrarPropiedades($id_propietario){//de uno especifico
+        $propiedades = $this->modelo->obtenerPropiedadesPorPropietario($id_propietario);
+        $propietario = $this->modelo->obtenerPropietarioPorId($id_propietario);
+        $this->vista->mostrarPropiedadesPorPropietario($propietario, $propiedades);
+    }
+
+    public function agregarPropietario() {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nombre = $_POST['nombre'];
+            $telefono = $_POST['telefono'];
+            $mail = $_POST['mail'];
+            $this->modelo->agregarPropietario($nombre, $telefono,$mail);
+            header('Location: ' . BASE_URL . 'propietarios');
+            exit();
+        } else {
+            $this->vista->mostrarFormularioAgregarPropietario();
+        }
+    }
+
+    public function mostrarForumularioEditarPropietario($id_propietario) {
+        $propietario = $this->modelo->obtenerPropietarioPorId($id_propietario);
+        if($propietario) {
+            $this->vista->mostrarForumularioEditarPropietario($propietario);
+        } else {
+            $this->vista->mostrarError("No se encontro propietario con el ID: " . $id_propietario);
+        }
+    }
+
+    public function editarPropietario($id_propietario) {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nombre = $_POST['nombre'];
+            $telefono = $_POST['telefono'];
+            $mail = $_POST['mail'];
+            $this->modelo->editarPropietario($id_propietario, $nombre, $telefono, $mail);
+            header('Location: ' . BASE_URL . 'propietarios');
+            exit();
+        }
+    }
+
+    // Eliminar propietario (solo si no tiene propiedades asociadas)
+    public function eliminarPropietario() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_propietario'])) {
+            $id_propietario = $_POST['id_propietario'];
+            $totalPropiedades = $this->modelo->contarPropiedadesPorPropietario($id_propietario);
+            if($totalPropiedades == 0) {
+                $this->modelo->eliminarPropietario($id_propietario);
+                header('Location:' . BASE_URL . 'propietarios');
+                exit();
+            } else {
+                $this->vista->mostrarError("No se puede eliminar el propietario porque tiene propiedades asociadas");
+            }
+        }
+    }
+}
---
+++ modelos/ModeloPropietario.php
@@ -0,0 +1,49 @@
+<?php
+require_once './config/config.php';
+public class ModeloPropietario {
+    private $db;
+
+    public function __construct() {
+        $this->db = new PDO('mysql:host=localhost;dbname=db_alquilertemp;charset=utf8', 'root', '');
+    }
+
+    public function obtenerPropietarios() {
+        $query = $this->db->prepare('SELECT * FROM propietarios');
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function obtenerPropietarioPorId($id_propietario) {
+        $query = $this->db->prepare('SELECT * FROM propietarios WHERE id_propietario = ?');
+        $query->execute([$id_propietario]);
+        return $query->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function agregarPropietario($nombre, $telefono, $mail) {
+        $query = $this->db->prepare('INSERT INTO propietarios (nombre, telefono, mail) VALUES (?, ?, ?)');
+        $query->execute([$nombre, $telefono, $mail]);
+    }
+
+    public function editarPropietario($id_usuario, $nombre, $telefono, $mail) {
+        $query = $this->db->prepare('UPDATE propietarios SET nombre = ?, telefono = ?, email = ? WHERE id_propietario = ?');
+        $query->execute([$nombre, $telefono, $mail, $id_propietario]);
+    }
+
+    public function eliminarPropietario($id_propietario) {
+        $query = $this->db->prepare('DELETE FROM propietarios WHERE id_propietario = ?');
+        $query->execute([$id_propietario]);
+    }
+
+    public function contarPropiedadesPorPropietario($id_propietario) {
+        $query = $this->db->prepare('SELECT COUNT(*) AS total FROM propiedades WHERE id_propietario_dk = ?');
+        $query->execute([$id_propietario]);
+        $result = $query->fetch(PDO::FETCH_OBJ);
+        return $result->total;
+    }
+   
+    public function obtenerPropiedadesPorPropietario($id_propietario) {
+        $query = $this->db->prepare('SELECT * FROM propiedades WHERE id_propietario_fk = ?');
+        $query->execute([$id_propietario]);
+        return $query->fetchAll(PDO::FETCH_OBJ);
+    }
+}
